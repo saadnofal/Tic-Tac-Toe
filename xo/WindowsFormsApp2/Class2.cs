d6d400b24caefada9e13c2e40e6ab2228d2c2064
saadnofal <mnt3lmswa10@gmail.com>
2024-05-11 14:18:51 +0300

    Add files via upload

---
+++ xo/WindowsFormsApp2/Class2.cs
@@ -0,0 +1,25 @@
+﻿namespace WindowsFormsApp2
+{
+   public class playerWin
+    {
+        int score;
+        public int Score
+        {
+            set { score = value; }
+            get { return score; }
+        }
+        public playerWin() { }
+        public playerWin(int score)
+        {
+            this.score = score;
+        }
+
+
+    }
+    public class playerDraw : playerWin
+    {
+        public playerDraw(int score):base(score) { }
+        
+    }
+
+}
